40ab839fa3de9b8dd9a509a94d4640557eba5c58
Benjamin Pena <benjaminpena@Benjamins-MacBook-Pro.local>
2016-11-20 12:39:12 -0700

    Created warmups repository

---
+++ Section 3 Warmup.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func isEven(num: Int) -> Bool {
+    if num % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+isEven(num: 2)
+isEven(num: 3)
